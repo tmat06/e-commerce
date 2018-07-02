94581fa40a618bdde5abc8e3041bfeda3242eaf6
tmat06 <timmy.a.matthews@gmail.com>
2018-07-02 12:07:53 -0600

    some layout design

---
+++ src/components/MiniRowDisplay.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class MiniRowDisplay extends Component {
+  render() {
+    return (
+      <div className="row-display">
+        <div className="mini-row-display-item" />
+        <div className="mini-row-display-item" />
+        <div className="mini-row-display-item" />
+        <div className="mini-row-display-item" />
+      </div>
+    );
+  }
+}
+
+export default MiniRowDisplay;
---
+++ src/components/RowDisplay.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class RowDisplay extends Component {
+  render() {
+    return (
+      <div className="row-display">
+        <div className="row-display-item" />
+        <div className="row-display-item" />
+        <div className="row-display-item" />
+        <div className="row-display-item" />
+        <div className="row-display-item" />
+      </div>
+    );
+  }
+}
+
+export default RowDisplay;
